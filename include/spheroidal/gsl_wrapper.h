5f477a5995a6f896cb835e0752d85450a623900a
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2023-04-07 11:42:48 -0600

    Fix some bugs, add some methods

--- include/spheroidal/gsl_wrapper.h
+++ include/spheroidal/gsl_wrapper.h
@@ -8,73 +8,85 @@
 namespace gsl
 {
 
-class vector
-{
-public:
-    // Ordinary constructors
-    vector();
-    vector(size_t n);
+    class vector
+    {
+    public:
+        // Ordinary constructors
+        vector();
+        vector(size_t n);
 
-    // Copy constructors
-    vector( gsl_vector * gvec_other );
-    vector( const vector& gvec_other );
+        // Copy constructors
+        vector(gsl_vector *gvec_other);
+        vector(const vector &gvec_other);
 
-    // Destructor
-    ~vector();
+        // Destructor
+        ~vector();
 
-    // Element access
-    double& operator()( size_t i );             // Getter
-    const double& operator()( size_t i ) const; // Setter
+        // Element access
+        double &operator()(size_t i);             // Getter
+        const double &operator()(size_t i) const; // Setter
 
-    size_t size() const;
+        //! \brief Assignment operator
+        vector &operator=(const vector &gvec_other);
 
-    void resize(size_t n);
+        size_t size() const;
 
-    void print() const;
-    
-    // Other functions to write later
-    // vector( initialization_list )
-    // Fix printing lol
-protected:
-    gsl_vector* gvec;
-    void free();
-    void calloc(size_t n);
-};
+        gsl_vector *get_gsl_ptr() const { return gvec; }
 
-class matrix
-{
-public:
-    // Ordinary constructors
-    matrix();
-    matrix(size_t n, size_t m);
+        void resize(size_t n);
+        void clear();
 
-    // Copy constructors
-    matrix( gsl_matrix * gmat_other );
-    matrix( const matrix& gmat_other );
+        void print() const;
 
-    // Destructor
-    ~matrix();
+        // Other functions to write later
+        // vector( initialization_list )
+        // Fix printing lol
+    protected:
+        gsl_vector *gvec;
+        void free();
+        void calloc(size_t n);
+    };
 
-    // Element access
-    double& operator()( size_t i, size_t j );             // Getter
-    const double& operator()( size_t i, size_t j ) const; // Setter
+    class matrix
+    {
+    public:
+        // Ordinary constructors
+        matrix();
+        matrix(size_t n, size_t m);
 
-    size_t size() const;
-    size_t nrows() const;
-    size_t ncols() const;
+        // Copy constructors
+        matrix(gsl_matrix *gmat_other);
+        matrix(const matrix &gmat_other);
 
-    void resize(size_t n, size_t m);
-    
-    void print() const;
-    
-    // Other functions to write later
-    // vector( initialization_list )
-    // Fix printing lol
-protected:
-    gsl_matrix* gmat;
-    void free();
-    void calloc(size_t n, size_t m);
-};
+        // Destructor
+        ~matrix();
+
+        // Element access
+        double &operator()(size_t i, size_t j);             // Getter
+        const double &operator()(size_t i, size_t j) const; // Setter
+
+        //! \brief Assignment operator
+        matrix &operator=(const matrix &gmat_other);
+
+        size_t size() const;
+        size_t nrows() const;
+        size_t ncols() const;
+
+        gsl_matrix *get_gsl_ptr() const { return gmat; }
+
+        void resize(size_t n, size_t m);
+        void clear();
+
+        void print() const;
+
+        // Other functions to write later
+        // vector( initialization_list )
+        // Do printing better
+    protected:
+        gsl_matrix *gmat;
+        void free();
+        void calloc(size_t n, size_t m);
+    };
 
 }
 
--- src/gsl_wrapper.cpp
+++ src/gsl_wrapper.cpp
@@ -4,134 +4,229 @@
 #include <fmt/core.h>
 
 /*------ Public Methods for gsl::vector ------*/
-gsl::vector::vector() : gvec( nullptr ) {}
-gsl::vector::vector( size_t n ) { this->calloc(n); }
-    
-gsl::vector::vector( gsl_vector * gvec_other )
+
+//! \brief Default constructor
+gsl::vector::vector() : gvec(nullptr) {}
+
+//! \brief Constructor with size
+gsl::vector::vector(size_t n) { this->calloc(n); }
+
+//! \brief Build a gsl::vector from a gsl_vector
+gsl::vector::vector(gsl_vector *gvec_other)
 {
-    this->calloc( gvec_other->size );
-    gsl_vector_memcpy( gvec, gvec_other );
+    this->calloc(gvec_other->size);
+    gsl_vector_memcpy(gvec, gvec_other);
 }
 
-gsl::vector::vector( const gsl::vector& gvec_other )
+//! \brief Copy constructor
+gsl::vector::vector(const gsl::vector &gvec_other)
 {
-    this->calloc( gvec_other.size() );
-    gsl_vector_memcpy( gvec, gvec_other.gvec );
+    this->calloc(gvec_other.size());
+    gsl_vector_memcpy(gvec, gvec_other.gvec);
 }
 
-gsl::vector::~vector() 
-{  
-    if( gvec == nullptr )
+//! \brief Destructor
+gsl::vector::~vector()
+{
+    if (gvec == nullptr)
         return;
-    gsl_vector_free( gvec ); 
+    gsl_vector_free(gvec);
 }
-    
-double& gsl::vector::operator()( size_t i ) { return *gsl_vector_ptr( gvec, i ); }
 
-const double& gsl::vector::operator()( size_t i ) const { return *gsl_vector_ptr( gvec, i ); }
+//! \brief Element getter
+const double &gsl::vector::operator()(size_t i) const { return *gsl_vector_ptr(gvec, i); }
 
-size_t gsl::vector::size() const { return gvec->size; };
+//! \brief Element setter
+double &gsl::vector::operator()(size_t i) { return *gsl_vector_ptr(gvec, i); }
 
-void gsl::vector::resize( size_t n )
+//! \brief Assignment operator
+gsl::vector &gsl::vector::operator=(const gsl::vector &gvec_other)
 {
-    // Don't free an empty vector
-    if( gvec != nullptr ) 
+    if (this == &gvec_other)
+        return *this;
+    this->resize(gvec_other.size());
+    gsl_vector_memcpy(gvec, gvec_other.gvec);
+    return *this;
+}
+
+//! \brief Size accessor
+size_t gsl::vector::size() const
+{
+    if (gvec == nullptr)
+        return 0;
+    return gvec->size;
+};
+
+//! \brief Resize the gsl::vector
+//! \note If n == 0, the vector is cleared
+void gsl::vector::resize(size_t n)
+{
+    if (n == 0)
     {
-        // Only allocate new memory if size is different
-        if( gvec->size == n ) return;
+        this->clear();
+        return;
+    }
+    if (gvec != nullptr)
+    {
+        if (gvec->size == n)
+            return;
         this->free();
     }
-    this->calloc( n );
+    this->calloc(n);
 }
 
-void gsl::vector::print() const 
+//! \brief Clear the vector and free the underlying gsl_vector
+void gsl::vector::clear()
 {
-    fmt::print( "[" );
-    for( int i = 0; i < gvec->size; ++i )
-        fmt::print( (i == 0) ? "{:g}" : ", {:g}", gsl_vector_get(gvec, i) );
-    fmt::print( "]\n" );
+    if (gvec == nullptr)
+        return;
+    this->free();
+}
+
+//! \brief Print the vector to stdout
+void gsl::vector::print() const
+{
+    fmt::print("[");
+    for (int i = 0; i < gvec->size; ++i)
+        fmt::print((i == 0) ? "{:g}" : ", {:g}", gsl_vector_get(gvec, i));
+    fmt::print("]\n");
 }
 
 /*------ Protected Methods for gsl::vector ------*/
+
+//! \brief Free memory for underlying gsl_vector
 void gsl::vector::free()
 {
-    if( gvec != nullptr ) return;
-    gsl_vector_free( gvec );
+    gsl_vector_free(gvec);
     gvec = nullptr;
 }
 
-void gsl::vector::calloc( size_t n )
-{
-    if( gvec != nullptr ) this->free();
-    gvec = gsl_vector_calloc( n );
-}
-
+/*!
+ * \brief Allocate memory for underlying gsl_vector
+ * \note This method allocates contiguous, zero-initialized memory.
+ *         This is slightly slower than using gsl_vector_alloc
+ */
+void gsl::vector::calloc(size_t n) { gvec = gsl_vector_calloc(n); }
 
 /*------ Public Methods for gsl::matrix ------*/
-gsl::matrix::matrix() : gmat( nullptr ) {}
-gsl::matrix::matrix( size_t n, size_t m ) { this->calloc(n, m); }
-    
-gsl::matrix::matrix( gsl_matrix * gmat_other )
+//! \brief Default constructor
+gsl::matrix::matrix() : gmat(nullptr) {}
+
+//! \brief Constructor with size
+gsl::matrix::matrix(size_t n, size_t m) { this->calloc(n, m); }
+
+//! \brief Build a gsl::matrix from a gsl_matrix
+gsl::matrix::matrix(gsl_matrix *gmat_other)
 {
-    this->calloc( gmat_other->size1, gmat_other->size2 );
-    gsl_matrix_memcpy( gmat, gmat_other );
+    this->calloc(gmat_other->size1, gmat_other->size2);
+    gsl_matrix_memcpy(gmat, gmat_other);
 }
 
-gsl::matrix::matrix( const gsl::matrix& gmat_other )
+//! \brief Copy constructor
+gsl::matrix::matrix(const gsl::matrix &gmat_other)
 {
-    this->calloc( gmat_other.nrows(), gmat_other.ncols() );
-    gsl_matrix_memcpy( gmat, gmat_other.gmat );
+    this->calloc(gmat_other.nrows(), gmat_other.ncols());
+    gsl_matrix_memcpy(gmat, gmat_other.gmat);
 }
 
-gsl::matrix::~matrix() 
-{  
-    if( gmat == nullptr )
+//! \brief Destructor
+gsl::matrix::~matrix()
+{
+    if (gmat == nullptr)
         return;
-    gsl_matrix_free( gmat ); 
+    gsl_matrix_free(gmat);
 }
-    
 
-double& gsl::matrix::operator()( size_t i, size_t j ) { return *gsl_matrix_ptr( gmat, i, j ); }
+//! \brief Element setter
+double &gsl::matrix::operator()(size_t i, size_t j) { return *gsl_matrix_ptr(gmat, i, j); }
 
-const double& gsl::matrix::operator()( size_t i, size_t j ) const { return *gsl_matrix_ptr( gmat, i, j ); }
+//! \brief Element getter
+const double &gsl::matrix::operator()(size_t i, size_t j) const { return *gsl_matrix_ptr(gmat, i, j); }
 
-size_t gsl::matrix::size() const { return gmat->size1 * gmat->size2; };
-size_t gsl::matrix::nrows() const { return gmat->size1; };
-size_t gsl::matrix::ncols() const { return gmat->size2; };
-
-void gsl::matrix::resize( size_t n, size_t m )
+//! \brief Assignment operator
+gsl::matrix &gsl::matrix::operator=(const gsl::matrix &gmat_other)
 {
+    if (this == &gmat_other)
+        return *this;
+    this->resize(gmat_other.nrows(), gmat_other.ncols());
+    gsl_matrix_memcpy(gmat, gmat_other.gmat);
+    return *this;
+}
+
+//! \brief Size accessor
+size_t gsl::matrix::size() const
+{
+    if (gmat == nullptr)
+        return 0;
+    return gmat->size1 * gmat->size2;
+};
+
+//! \brief Number of rows accessor
+size_t gsl::matrix::nrows() const
+{
+    if (gmat == nullptr)
+        return 0;
+    return gmat->size1;
+}
+
+//! \brief Number of columns accessor
+size_t gsl::matrix::ncols() const
+{
+    if (gmat == nullptr)
+        return 0;
+    return gmat->size2;
+}
+
+//! \brief Resize the gsl::matrix
+void gsl::matrix::resize(size_t n, size_t m)
+{
+    if ( (n == 0) || (m == 0) )
+    {
+        this->clear();
+        return;
+    }
     // Don't free an empty vector
-    if( gmat != nullptr ) 
+    if (gmat != nullptr)
     {
         // Only allocate new memory if size is different
-        if( gmat->size1 == n && gmat->size2 == m ) return;
+        if (gmat->size1 == n && gmat->size2 == m)
+            return;
         this->free();
     }
-    this->calloc( n, m );
+    this->calloc(n, m);
 }
 
-void gsl::matrix::print() const 
+//! \brief Clear the gsl::matric and free the underlying gsl_matrix
+void gsl::matrix::clear()
 {
-    for( int i = 0; i < gmat->size1; ++i )
+    if (gmat == nullptr)
+        return;
+    this->free();
+}
+
+//! \brief Print the matrix to stdout
+void gsl::matrix::print() const
+{
+    for (int i = 0; i < gmat->size1; ++i)
     {
-        fmt::print( (i == 0) ? "[" : " " );
-        for( int j = 0; j < gmat->size2; ++j )
-            fmt::print( (j == 0) ? "{: 9g}" : " {: 9g}", gsl_matrix_get(gmat, i, j) );
-        fmt::print( (i == (gmat->size1 - 1) ? "]\n" : "\n") );
+        fmt::print((i == 0) ? "[" : " ");
+        for (int j = 0; j < gmat->size2; ++j)
+            fmt::print((j == 0) ? "{: 9g}" : " {: 9g}", gsl_matrix_get(gmat, i, j));
+        fmt::print((i == (gmat->size1 - 1) ? "]\n" : "\n"));
     }
 }
 
 /*------ Protected Methods for gsl::matrix ------*/
+//! \brief Free memory for underlying gsl_matrix
 void gsl::matrix::free()
 {
-    if( gmat != nullptr ) return;
-    gsl_matrix_free( gmat );
+    gsl_matrix_free(gmat);
     gmat = nullptr;
 }
 
-void gsl::matrix::calloc( size_t n, size_t m )
-{
-    if( gmat != nullptr ) this->free();
-    gmat = gsl_matrix_calloc( n, m );
-}
+/*!
+ * \brief Allocate memory for underlying gsl_matrix
+ * \note This method allocates contiguous, zero-initialized memory.
+ *       This is slightly slower than using gsl_matrix_alloc
+ */
+void gsl::matrix::calloc(size_t n, size_t m) { gmat = gsl_matrix_calloc(n, m); }
