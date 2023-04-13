6dadb6bef86ae0202844e08a515f5fe8076d7891
lcrowder <lecr8188@colorado.edu>
2023-04-12 22:29:58 -0600

    trying to convert to gsl vectors in legendre_otc

--- examples/test_legendre_otc.cpp
+++ examples/test_legendre_otc.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <stdio.h>  
 #include <math.h> 
+#include <gsl_wrapper/utils.hpp>
+#include <gsl_wrapper/core.h>
 using namespace std;
 
 
@@ -15,86 +17,92 @@ int main(int argc, char *argv[])
     double ub=atof(argv[3]);
     int N=atoi(argv[4]);
 
-    double h=(ub-ua)/(N-1);
-    vector<double> u;
-    // for (double ui = ua; ui <= ub; ui+=h)
-    //     {u.push_back(ui);}
+    // double h=(ub-ua)/(N-1);
+    // vector<double> u;
+    // // for (double ui = ua; ui <= ub; ui+=h)
+    // //     {u.push_back(ui);}
 
-    double uj=ua;
-    for (int j=0; j<N; ++j)
-    {
-        u.push_back(ua+j*h);
-        // cout << "u[" << j << "]=" << uj << endl;
-    }
+    // double uj=ua;
+    // for (int j=0; j<N; ++j)
+    // {
+    //     u.push_back(ua+j*h);
+    //     // cout << "u[" << j << "]=" << uj << endl;
+    // }
+
+    gsl::vector u = gsl::linspace(ua,ub,N);
 
     cout << "length of u is " << u.size() << endl;
-    cout << "Last u-value is " << u[u.size()-1] << endl;
+    cout << "Last u-value is " << u(u.size()-1) << endl;
+
+    gsl::matrix P;
+    legendre_otc(p, u, P);
+    cout << "Last entry of P is " << P.get((p+1)*(p+1)-1,N-1) << endl;
     
-    int sp=(p+1)*(p+1);
+    // int sp=(p+1)*(p+1);
 
-    // vector<double> h=cont_frac(1,1,u);
+    // // vector<double> h=cont_frac(1,1,u);
 
-    // cout << endl;
-    // for (int i=0; i< 3; i++) {cout << h[i] << endl;}
+    // // cout << endl;
+    // // for (int i=0; i< 3; i++) {cout << h[i] << endl;}
 
     
-    vector<vector<vector<double> > > PQdPdQ;
-    PQdPdQ=legendre_otc(p,u,1,1,1);
+    // vector<vector<vector<double> > > PQdPdQ;
+    // PQdPdQ=legendre_otc(p,u,1,1,1);
 
-    vector<vector<double> > P = PQdPdQ[0];
-    vector<vector<double> > Q = PQdPdQ[1];
-    vector<vector<double> > dP = PQdPdQ[2];
-    vector<vector<double> > dQ = PQdPdQ[3];
+    // vector<vector<double> > P = PQdPdQ[0];
+    // vector<vector<double> > Q = PQdPdQ[1];
+    // vector<vector<double> > dP = PQdPdQ[2];
+    // vector<vector<double> > dQ = PQdPdQ[3];
 
-    // Open a file for writing
-    ofstream Pfile("data/P.txt");
-    ofstream Qfile("data/Q.txt");
-    ofstream dPfile("data/dP.txt");
-    ofstream dQfile("data/dQ.txt");
+    // // Open a file for writing
+    // ofstream Pfile("data/P.txt");
+    // ofstream Qfile("data/Q.txt");
+    // ofstream dPfile("data/dP.txt");
+    // ofstream dQfile("data/dQ.txt");
 
-    // Set precision to 16 decimal places
-    Pfile << fixed << setprecision(16);
-    Qfile << fixed << setprecision(16);
-    dPfile << fixed << setprecision(16);
-    dQfile << fixed << setprecision(16);
+    // // Set precision to 16 decimal places
+    // Pfile << fixed << setprecision(16);
+    // Qfile << fixed << setprecision(16);
+    // dPfile << fixed << setprecision(16);
+    // dQfile << fixed << setprecision(16);
 
-    // Check if files are open
-    if (!Pfile.is_open()) {
-        cerr << "Unable to open P file" << std::endl;
-        return 1;
-    }
-    if (!Qfile.is_open()) {
-        cerr << "Unable to open Q file" << std::endl;
-        return 1;
-    }
-    if (!dPfile.is_open()) {
-        cerr << "Unable to open dP file" << std::endl;
-        return 1;
-    }
-    if (!dQfile.is_open()) {
-        cerr << "Unable to open dQ file" << std::endl;
-        return 1;
-    }
+    // // Check if files are open
+    // if (!Pfile.is_open()) {
+    //     cerr << "Unable to open P file" << std::endl;
+    //     return 1;
+    // }
+    // if (!Qfile.is_open()) {
+    //     cerr << "Unable to open Q file" << std::endl;
+    //     return 1;
+    // }
+    // if (!dPfile.is_open()) {
+    //     cerr << "Unable to open dP file" << std::endl;
+    //     return 1;
+    // }
+    // if (!dQfile.is_open()) {
+    //     cerr << "Unable to open dQ file" << std::endl;
+    //     return 1;
+    // }
 
-    // Write the vector to the files
-    for (int i=0; i<sp; ++i) {
-        for (int j=0; j<N; ++j) {
-            Pfile << scientific << P[i][j] << " ";
-            Qfile << scientific << Q[i][j] << " ";
-            dPfile << scientific << dP[i][j] << " ";
-            dQfile << scientific << dQ[i][j] << " ";
-        }
-        Pfile << endl;
-        Qfile << endl;
-        dPfile << endl;
-        dQfile << endl;
-    }
+    // // Write the vector to the files
+    // for (int i=0; i<sp; ++i) {
+    //     for (int j=0; j<N; ++j) {
+    //         Pfile << scientific << P[i][j] << " ";
+    //         Qfile << scientific << Q[i][j] << " ";
+    //         dPfile << scientific << dP[i][j] << " ";
+    //         dQfile << scientific << dQ[i][j] << " ";
+    //     }
+    //     Pfile << endl;
+    //     Qfile << endl;
+    //     dPfile << endl;
+    //     dQfile << endl;
+    // }
 
-    // Close the files
-    Pfile.close();
-    Qfile.close();
-    dPfile.close();
-    dQfile.close();
+    // // Close the files
+    // Pfile.close();
+    // Qfile.close();
+    // dPfile.close();
+    // dQfile.close();
 
     
 
